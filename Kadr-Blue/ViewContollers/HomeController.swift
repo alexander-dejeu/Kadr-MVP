f517d0f4aef4390cab71d45b64066464b19fb258
Alexander Dejeu <alexdejeu@gmail.com>
2016-10-12 09:50:34 -0700

    set up project in Xcode, started storyboarding homescreen

---
+++ Kadr-Blue/ViewContollers/HomeController.swift
@@ -0,0 +1,38 @@
+//
+//  HomeController.swift
+//  Kadr-Blue
+//
+//  Created by Alex Dejeu on 10/12/16.
+//  Copyright © 2016 Kadr. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    
+    //Customer Support Button Radius = 3pt
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
