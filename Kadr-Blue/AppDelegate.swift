129487bf3146c727c75d7f1b30761e934406e987
Alexander Dejeu <alexdejeu@gmail.com>
2016-10-29 11:43:24 -0700

    check out stripe pods pls

--- Kadr-Blue/AppDelegate.swift
+++ Kadr-Blue/AppDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 import Firebase
 import Stripe
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
---
+++ Kadr-Blue/Kadr-Blue-Bridging-Header.h
@@ -0,0 +1,14 @@
+////
+////  Kadr-Blue-Bridging-Header.h
+////  Kadr-Blue
+////
+////  Created by Alex Dejeu on 10/29/16.
+////  Copyright © 2016 Kadr. All rights reserved.
+////
+//
+//#ifndef Kadr_Blue_Bridging_Header_h
+//#define Kadr_Blue_Bridging_Header_h
+
+#import <Stripe/Stripe.h>
+
+//#endif /* Kadr_Blue_Bridging_Header_h */
